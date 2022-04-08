737fd7ece5026e439c0a8f640c20dc33b5cd1459
Максим Давыдов <maxwest2002@outlook.com>
2022-04-08 19:03:00 +0700

    ПОЧТИ ГОТОВОЕ 1. Добавлен второй пикчбокс, картинки и вырублен доступ к ним пока не подключен клиент 2. Повышен размер сообщения засчет streamwriter/streamreader 3. добавлено сообщение клиенту от сервера "Нераспознанный запрос" 4. пофикшено изменение тексбокса2 при отправки не-папки 5. Склеено с отрублением от сервера

--- ClientServerApp/Form1.cs
+++ ClientServerApp/Form1.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
-using System.IO;
+using System.Windows.Forms;
 
 namespace ClientServerApp
 {
     public partial class Form1 : Form
     {
         static int portNumber = 8080;
-        Thread ThreadingClient;
         public Form1()
         {
             InitializeComponent();
@@ -35,6 +29,7 @@ namespace ClientServerApp
                 serverSocket = new TcpListener(IPAddress.Parse(textBox1.Text), portNumber);
                 ThreadingServer = new Thread(StartServer);
                 ThreadingServer.Start();
+
                 ServerOffButton.Enabled = true;
                 ServerOnButton.Enabled = false;
                 ConnectButton.Enabled = true;
@@ -46,55 +41,66 @@ namespace ClientServerApp
         }
         private void RecieveMessage(string message)
         {
-            ServerTextBox.Text += $"{message}. {DateTime.Now}\n";
+            ServerTextBox.Text += $"{message}. {DateTime.Now}\n\n\n\n";
         }
-        
         private void StartServer()
         {
             Action<string> DelegateRecieveMessage = RecieveMessage;
             serverSocket.Start();
             Invoke(DelegateRecieveMessage, $"Сервер включён в {DateTime.Now}. Порт в режиме ожидания соединения...");
             clientSocket = serverSocket.AcceptTcpClient();
-            ServerTextBox.Text += $"Клиент подключен с адреса: {(IPEndPoint)clientSocket.Client.RemoteEndPoint} {DateTime.Now}\n";
-            SendDrives();
+            bool disksSent = false;
             while (clientSocket.Connected)
             {
+                if (!disksSent)
+                {
+                    ServerTextBox.Text += $"Клиент подключен с адреса: {(IPEndPoint)clientSocket.Client.RemoteEndPoint} {DateTime.Now}\n\n";
+                    SendDrives();
+                    disksSent = true;
+                }
                 try
                 {
                     NetworkStream networkStream = clientSocket.GetStream();
-                    byte[] bytesFrom = new byte[1024];
-                    networkStream.Read(bytesFrom, 0, 1024);
-                    string dataFromClient = Encoding.UTF8.GetString(bytesFrom);
+                    StreamReader reader = new StreamReader(networkStream);
+                    string dataFromClient = reader.ReadLine();
+
                     dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
                     if (dataFromClient == "Диски")
                     {
                         SendDrives();
+                        ServerTextBox.Text += $"Сервер получил сообщение от клиента: {dataFromClient}. {DateTime.Now}\n\n";
+                    }
+                    else if (dataFromClient == "Отключение от сервера:...")
+                    {
+                        throw new Exception("Клиент отключился");
                     }
                     else
-					{
-                        dataFromClient = dataFromClient.Trim('\0');
-						if (Directory.Exists(dataFromClient))
-						{
+                    {
+                        ServerTextBox.Text += $"Сервер получил сообщение от клиента: {dataFromClient}. {DateTime.Now}\n\n";
+                        if (Directory.Exists(dataFromClient))
+                        {
                             SendDirectories(dataFromClient);
-						}
+                        }
                         if (File.Exists(dataFromClient))
-						{
+                        {
                             if (dataFromClient.EndsWith(".txt"))
                             {
                                 SendText(dataFromClient);
                             }
-							else
-							{
-                                //
-							}
+                            else
+                            {
+                                StreamWriter writer = new StreamWriter(networkStream);
+                                writer.WriteLine("Этот файл распознать нельзя!");
+                                writer.Flush();
+                            }
                         }
                     }
-                    ServerTextBox.Text += $"Сервер получил сообщение от клиента: {dataFromClient}. {DateTime.Now}\n";
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    Invoke(DelegateRecieveMessage, $"Клиент отключился в {DateTime.Now}. Порт в режиме ожидания соединения...");
+                    disksSent = false;
+                    //MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Invoke(DelegateRecieveMessage, $"{ex.Message}. Перезагрузка сервера... {DateTime.Now}. Порт в режиме ожидания соединения..."); serverSocket.Stop();
                     serverSocket.Stop();
                     serverSocket.Start();
                     clientSocket = serverSocket.AcceptTcpClient();
@@ -107,67 +113,90 @@ namespace ClientServerApp
             foreach (var drive in DriveInfo.GetDrives())
             {
                 if (drives != "")
+                {
                     drives += ",";
+                }
+
                 drives += drive.Name;
             }
             string message = "Диски: " + drives;
-            byte[] listBytes = Encoding.UTF8.GetBytes(message);
             NetworkStream networkStream = clientSocket.GetStream();
-            networkStream.Write(listBytes, 0, listBytes.Length);
-            networkStream.Flush();
+
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.WriteLine(message);
+            writer.Flush();
         }
         private void SendDirectories(string path)
-		{
+        {
             string dirs = "";
             foreach (var item in Directory.GetDirectories(path))
             {
                 if (dirs != "")
+                {
                     dirs += ",";
+                }
+
                 dirs += Path.GetFileName(item);
             }
             foreach (var item in Directory.GetFiles(path))
             {
                 if (dirs != "")
+                {
                     dirs += ",";
+                }
+
                 dirs += Path.GetFileName(item);
             }
             string message = "Каталоги и файлы: " + dirs;
-            byte[] listBytes = Encoding.UTF8.GetBytes(message);
+
             NetworkStream networkStream = clientSocket.GetStream();
-            networkStream.Write(listBytes, 0, listBytes.Length);
-            networkStream.Flush();
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.WriteLine(message);
+            writer.Flush();
         }
         private async void SendText(string path)
-		{
-			using (StreamReader streamReader = new StreamReader(path))
-			{
-                
+        {
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+
                 string text = await streamReader.ReadToEndAsync();
                 string message = "Содержимое текстового файла: " + text;
                 byte[] listBytes = Encoding.UTF8.GetBytes(message);
                 NetworkStream networkStream = clientSocket.GetStream();
-                networkStream.Write(listBytes, 0, listBytes.Length);
-                networkStream.Flush();
+                StreamWriter writer = new StreamWriter(networkStream);
+                writer.WriteLine(message);
+                writer.Flush();
             }
-		}
-        private void ServerOffButton_Click(object sender, EventArgs e)
+        }
+        private void TurnOffServer()
         {
-            ServerTextBox.Text += $"Сервер отключен {DateTime.Now}\n";
+            if (clientSocket != null)
+            {
+                NetworkStream networkStream = clientSocket.GetStream();
+                StreamWriter writer = new StreamWriter(networkStream);
+                writer.WriteLine("Сервер отключен: ");
+                writer.Flush();
+            }
             serverSocket.Stop();
             ThreadingServer.Suspend();
+        }
+        private void ServerOffButton_Click(object sender, EventArgs e)
+        {
+            TurnOffServer();
+            ServerTextBox.Text += $"Сервер отключен {DateTime.Now}\n\n";
             ServerOnButton.Enabled = true;
-
             ServerOffButton.Enabled = false;
             DisconnectButton.Enabled = false;
-            SendToServerButton.Enabled = false;
             ConnectButton.Enabled = false;
-            SendToClientButton.Enabled = false;
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
         }
         #endregion
 
         #region [ CLIENT  ]
+        Thread ThreadingClient;
         TcpClient tcpClient;
-        string directory;
+        string path;
         List<string> directories;
         private void ConnectButton_Click(object sender, EventArgs e)
         {
@@ -176,13 +205,13 @@ namespace ClientServerApp
                 directories = new List<string>();
                 tcpClient = new TcpClient();
                 tcpClient.Connect(IPAddress.Parse(textBox1.Text), portNumber);
-                ClientTextBox.Text += $"Подключено к серверу! {DateTime.Now}\n";
+                ClientTextBox.Text += $"Подключено к серверу! {DateTime.Now}\n\n";
                 ThreadingClient = new Thread(AcceptResponses);
                 ThreadingClient.Start();
                 DisconnectButton.Enabled = true;
-                SendToServerButton.Enabled = true;
+                pictureBox1.Enabled = true;
+                pictureBox2.Enabled = true;
                 ConnectButton.Enabled = false;
-                SendToClientButton.Enabled = true;
             }
             catch (Exception ex)
             {
@@ -196,14 +225,13 @@ namespace ClientServerApp
                 try
                 {
                     NetworkStream clientStream = tcpClient.GetStream();
-                    byte[] bb = new byte[1024];
+                    StreamReader reader = new StreamReader(clientStream);
+                    string serverResponse = reader.ReadLine();
 
-                    clientStream.Read(bb, 0, 1024);
-                    string serverResponse = Encoding.UTF8.GetString(bb);
-                    ClientTextBox.Text += $"Клиент получил ответ от сервера: {serverResponse}";
-                    ClientTextBox.Text += $" {DateTime.Now}\n";
-					if (serverResponse.Contains("Диски: "))
+                    if (serverResponse.Contains("Диски: "))
                     {
+                        ClientTextBox.Text += $"Клиент получил ответ от сервера: {serverResponse}";
+                        ClientTextBox.Text += $" {DateTime.Now}\n\n";
                         serverResponse = serverResponse.Replace("Диски: ", "");
                         string[] dirs = serverResponse.Split(',');
                         directories.Clear();
@@ -215,6 +243,8 @@ namespace ClientServerApp
                     }
                     else if (serverResponse.Contains("Каталоги и файлы: "))
                     {
+                        ClientTextBox.Text += $"Клиент получил ответ от сервера: {serverResponse}";
+                        ClientTextBox.Text += $" {DateTime.Now}\n\n";
                         serverResponse = serverResponse.Replace("Каталоги и файлы: ", "");
                         string[] dirs = serverResponse.Split(',');
                         directories.Clear();
@@ -225,12 +255,19 @@ namespace ClientServerApp
                         UpdateListBox();
                     }
                     else if (serverResponse.Contains("Содержимое текстового файла: "))
-					{
-                        
+                    {
+                        ClientTextBox.Text += $"Клиент получил ответ от сервера: {serverResponse}";
+                        ClientTextBox.Text += $" {DateTime.Now}\n\n";
+                    }
+                    else if (serverResponse.Contains("Сервер отключен: "))
+                    {
+                        ClientTextBox.Text += $"Сервер отключен в {DateTime.Now}\n\n";
+                        ThreadingClient.Suspend();
                     }
                     else
-					{
-                        MessageBox.Show("Нераспознанное сообщение с сервера");
+                    {
+                        ClientTextBox.Text += $"Клиент получил ответ от сервера: {serverResponse}\n\n";
+                        ClientTextBox.Text += $" {DateTime.Now}\n\n";
                     }
                 }
                 catch (Exception ex)
@@ -240,71 +277,105 @@ namespace ClientServerApp
             }
         }
         private void UpdateListBox()
-		{
+        {
             listBox1.Items.Clear();
-			foreach (var item in directories)
-			{
+            foreach (var item in directories)
+            {
                 string temp = item.Trim('\0');
                 listBox1.Items.Add(temp);
-			}
-            listBox1.Items.Add(@"C:\Users");
-		}
+            }
+        }
         private void SendToServerButton_Click(object sender, EventArgs e)
         {
-            if (directory == null || directory.ToString() == "")
+            if (listBox1.SelectedItems.Count == 1)
             {
-                string path = listBox1.SelectedItem as string;
-                directory = path;
+                if (path == null || path.ToString() == "")
+                {
+                    string path = listBox1.SelectedItem as string;
+                    this.path = path;
+                }
+                else
+                {
+
+                    string path = listBox1.SelectedItem.ToString();
+                    this.path = Path.Combine(this.path, path);
+                }
+                if (Directory.Exists(Path.Combine(path, path)))
+                {
+                    textBox2.Text = path;
+                }
+                SendToServer(path);
             }
-            else
-            {
-                string path = listBox1.SelectedItem.ToString();
-                directory = Path.Combine(directory, path);
-            }
-            SendToServer(directory);
+
+        }
+        private void Disconnect()
+        {
+            NetworkStream clientStream = tcpClient.GetStream();
+            string message = "Отключение от сервера:..." + "$";
+            StreamWriter writer = new StreamWriter(clientStream);
+            writer.WriteLine(message);
+            writer.Flush();
+            tcpClient.Close();
+            ThreadingClient.Suspend();
         }
         private void DisconnectButton_Click(object sender, EventArgs e)
         {
             try
             {
-                tcpClient.Close();
-                ThreadingClient.Suspend();
-                ClientTextBox.Text += $"Клиент отключен от сервера в {DateTime.Now}\n";
+                Disconnect();
+                ClientTextBox.Text += $"Клиент отключен от сервера в {DateTime.Now}\n\n";
                 DisconnectButton.Enabled = false;
-                SendToServerButton.Enabled = false;
                 ConnectButton.Enabled = true;
-                SendToClientButton.Enabled = false;
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = false;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-		#endregion
         private void SendToServer(string text)
-		{
+        {
             try
             {
                 string message = text + "$";
                 NetworkStream clientStream = tcpClient.GetStream();
-                UTF8Encoding encoding = new UTF8Encoding();
-                byte[] bytes = encoding.GetBytes(message);
-                textBox2.Text = directory;
-                ClientTextBox.Text += $"Передача {message.Substring(0, message.IndexOf("$"))} серверу... {DateTime.Now}\n";
-                clientStream.Write(bytes, 0, bytes.Length);
+                ClientTextBox.Text += $"Передача {message.Substring(0, message.IndexOf("$"))} серверу... {DateTime.Now}\n\n";
+                StreamWriter writer = new StreamWriter(clientStream);
+                writer.WriteLine(message);
+                writer.Flush();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-		private void pictureBox1_Click(object sender, EventArgs e)
-		{
-            directory = Path.GetDirectoryName(directory);
-            if (directory == null)
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            path = Path.GetDirectoryName(path);
+            if (path == null)
+            {
                 SendToServer("Диски");
+            }
             else
-                SendToServer(directory);
+            {
+                SendToServer(path);
+            }
+
+            textBox2.Text = path;
         }
-	}
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            path = "";
+            SendToServer("Диски");
+            textBox2.Text = path;
+        }
+        #endregion
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            TurnOffServer();
+            Disconnect();
+            Application.Exit();
+        }
+    }
 }
